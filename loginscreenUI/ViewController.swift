1ef7e6c1820e5c3dec9e187606a798c4d7fc8087
Jessica Ngo <jessican-go@hotmail.com>
2018-07-08 00:53:53 -0400

    Initial Commit

---
+++ loginscreenUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  loginscreenUI
+//
+//  Created by Jessica Ngo on 2018-07-08.
+//  Copyright © 2018 Jessica Ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
